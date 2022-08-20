bf28ab14203749d5847ad379df88cbd599cbf01f
Paul Beckford <beckfordp@btinternet.com>
2022-08-20 17:38:27 +0100

    Green bar: add default constructor to Scoreboard

--- app/src/main/java/com/sportradar/Scoreboard.java
+++ app/src/main/java/com/sportradar/Scoreboard.java
@@ -9,6 +9,10 @@ public class Scoreboard {
         this.matches = matches;
     }
 
+    public Scoreboard() {
+        this.matches = new ArrayList<>();
+    }
+
     public void startGame(Match aMatch) {
         this.matches.add(aMatch);
     }
