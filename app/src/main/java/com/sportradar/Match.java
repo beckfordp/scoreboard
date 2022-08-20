550fbd62561550b3145585cd79e746f3d4ab54ea
Paul Beckford <beckfordp@btinternet.com>
2022-08-20 17:53:31 +0100

    Green bar: Add score attribute to Match class.

--- app/src/main/java/com/sportradar/Match.java
+++ app/src/main/java/com/sportradar/Match.java
@@ -1,11 +1,18 @@
 package com.sportradar;
 
+import java.util.*;
+
 public class Match {
     private final String homeTeam;
     private final String awayTeam;
+    List<Integer> score = new ArrayList<>(Arrays.asList(0, 0));
 
     public Match(String homeTeam, String awayTeam) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
+    }
+
+    public List<Integer> score() {
+        return score;
     }
 }
