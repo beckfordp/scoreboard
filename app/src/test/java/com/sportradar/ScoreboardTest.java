9b09d0da79f772b1b83797dbc1ebd937ea85b6fe
Paul Beckford <beckfordp@btinternet.com>
2022-08-20 17:35:56 +0100

    T: new scoreboard has no matches by default

--- app/src/test/java/com/sportradar/ScoreboardTest.java
+++ app/src/test/java/com/sportradar/ScoreboardTest.java
@@ -6,10 +6,16 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//TODO delete gradle template generated code
-//TODO T: can start a game
+
+// TODO T: new scoreboard has no games
+// TODO T: new Game score 0-0
 
 class ScoreboardTest {
+    @Test
+    void newScoreBoardHasNoMatchesByDefault() {
+        Scoreboard sb = new Scoreboard();
+        assertEquals(0, sb.matches.size());
+    }
 
     @Test
     void canStartAGame() {
