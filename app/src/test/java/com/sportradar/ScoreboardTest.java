866ab46f52dadbebdf67dcf7bbf091146ad3bc07
Paul Beckford <beckfordp@btinternet.com>
2022-08-20 17:18:52 +0100

    T: canStartGame. Notice that this test doesn't even compile... I generate the classes I need from the test using the IDE

---
+++ app/src/test/java/com/sportradar/ScoreboardTest.java
@@ -0,0 +1,23 @@
+package com.sportradar;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//TODO delete gradle template generated code
+//TODO T: can start a game
+
+class ScoreboardTest {
+
+    @Test
+    void canStartAGame() {
+        List<Match> matches = new ArrayList<>();
+        Scoreboard sb = new Scoreboard(matches);
+
+        sb.startGame(new Match("Mexico", "Canada");
+        assertEquals(1, sb.matches.size());
+    }
+
+}
